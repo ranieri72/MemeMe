ae78f581a47b57404be25afd7f2b0cbdccf1433d
Ranieri Aguiar <ranieri.aguiar@serttel.com.br>
2019-01-23 23:58:39 -0300

    Ajustes iniciais

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -2,19 +2,55 @@
 //  ViewController.swift
 //  MemeMe
 //
-//  Created by MTZ-5555-TRANSP on 23/01/19.
+//  Created by Ranieri Aguiar on 23/01/19.
 //  Copyright © 2019 Ranieri. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var topLabel: UITextField!
+    @IBOutlet var bottomLabel: UITextField!
+    @IBOutlet var btnCamera: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        btnCamera.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        let memeTextAttributes:[NSAttributedString.Key:Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.white,
+            NSAttributedString.Key.foregroundColor: UIColor.black,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth: 1.0]
+        topLabel.defaultTextAttributes = memeTextAttributes
+        bottomLabel.defaultTextAttributes = memeTextAttributes
     }
-
-
+    
+    @IBAction func btnCameraAction(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func btnGalleryAction(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            image.image = selectedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
-
