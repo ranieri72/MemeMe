ad8fe24476c1cfab695d77ef4aa802ea96c9b7ae
Ranieri Aguiar <ranieri.aguiar@serttel.com.br>
2019-01-25 00:12:26 -0300

    Ajustes no projeto

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Ranieri Aguiar on 24/01/19.
+//  Copyright © 2019 Ranieri. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    var textTop: String
+    var textBottom: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet var bottomLabel: UITextField!
     @IBOutlet var btnCamera: UIBarButtonItem!
     
+    // MARK: viewController
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,6 +30,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         bottomLabel.defaultTextAttributes = memeTextAttributes
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    // MARK: save
+    func save() {
+        let memedImage = generateMemedImage()
+        _ = Meme(textTop: topLabel.text!, textBottom: bottomLabel.text!, originalImage: image.image!, memedImage: memedImage)
+    }
+    
+    func generateMemedImage() -> UIImage {
+        navigationController?.isToolbarHidden = true
+        navigationController?.isNavigationBarHidden = true
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        navigationController?.isToolbarHidden = false
+        navigationController?.isNavigationBarHidden = false
+        return memedImage
+    }
+    
+    // MARK: button
     @IBAction func btnCameraAction(_ sender: UIBarButtonItem) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
@@ -43,6 +76,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
     }
     
+    // MARK: imagePicker
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             image.image = selectedImage
@@ -52,5 +86,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: keyboard
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
     }
 }
